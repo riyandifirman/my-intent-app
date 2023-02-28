bdf10d758b93a706b3d299eeb2ccb2d75c013740
riyandifirman <riyandi.firman08@gmail.com>
2023-02-28 21:07:24 +0700

    add some comment

--- app/src/main/java/com/riyandifirman/myintentapp/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/myintentapp/MainActivity.kt
@@ -21,13 +21,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_move_activity -> {
+                // Intent tanpa membawa data
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
             }
 
             R.id.btn_move_activity_data -> {
+                // Intent dengan membawa data
                 val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                // Memasukkan data ke intent dengan key EXTRA_NAME dan value "Riyandi Firman"
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Riyandi Firman")
+                // Memasukkan data ke intent dengan key Extra_AGE dan value 20
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
                 startActivity(moveWithDataIntent)
             }
--- app/src/main/java/com/riyandifirman/myintentapp/MoveWithDataActivity.kt
+++ app/src/main/java/com/riyandifirman/myintentapp/MoveWithDataActivity.kt
@@ -6,20 +6,26 @@ import android.widget.TextView
 
 class MoveWithDataActivity : AppCompatActivity() {
 
+    // Deklarasi companion object untuk menampung key dan value
     companion object {
         const val EXTRA_AGE = "extra_age"
         const val EXTRA_NAME = "extra_name"
     }
 
+    // Method onCreate() untuk menampilkan data yang dikirimkan dari MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_data)
 
+        // Mencari view dengan id tv_data_received
         val tvDataReceived: TextView = findViewById(R.id.tv_data_received)
 
+        // Membuat variabel name dan age untuk menampung data yang dikirimkan dari MainActivity
+        // dengan key EXTRA_NAME dan EXTRA_AGE default value 0
         val name = intent.getStringExtra(EXTRA_NAME)
         val age = intent.getIntExtra(EXTRA_AGE, 0)
 
+        // Menampilkan data yang dikirimkan dari MainActivity ke TextView
         val text = "Name: $name, Your age: $age"
         tvDataReceived.text = text
     }
